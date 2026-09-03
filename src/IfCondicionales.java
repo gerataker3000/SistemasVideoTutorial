92fc709f43072e13a527b6392e6889372054673f
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-09-02 21:45:49 -0600

    (update): IF | se cambio de if a elif

--- src/IfCondicionales.java
+++ src/IfCondicionales.java
@@ -1,10 +1,14 @@
 public class IfCondicionales {
     public static void main(String[] args) {
-        int calificacion = 70;
-        if(calificacion >=70){
-            System.out.println("Aprobado");
+        int calificacion = 92;
+        if(calificacion >=70 && calificacion <= 80){
+            System.out.println("Regular");
+        }else if(calificacion >=81 && calificacion <=90){
+            System.out.println("Buena");
+        } else if (calificacion >=91 && calificacion <=100) {
+            System.out.println("Excelente");
         }else{
-            System.out.println("Ustedes puedes");
+            System.out.println("Ustedes pueden");
         }
     }
 }
