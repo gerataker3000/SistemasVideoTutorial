8af7762988aa682b54ffe9b317bb2517e5b365c3
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-09-02 21:42:59 -0600

    Initial commit

---
+++ src/IfCondicionales.java
@@ -0,0 +1,10 @@
+public class IfCondicionales {
+    public static void main(String[] args) {
+        int calificacion = 70;
+        if(calificacion >=70){
+            System.out.println("Aprobado");
+        }else{
+            System.out.println("Ustedes puedes");
+        }
+    }
+}
